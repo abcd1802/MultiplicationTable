4ac89fac3b0f489b1e3524d66497a495077f6768
SAMSUNG <SAMSUNG@SAMSUNG-PC>
2021-03-03 15:52:37 -0500

    six time table and seven time table

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,18 +1,89 @@
+  
 import java.util.Scanner;
 
 public class MultiplicationTable{
 	public static void main(String[] args) {
-		System.out.println("구구단 중 출력하고 싶은 단은? ");
-		Scanner scanner = new Scanner(System.in);
-		int input = scanner.nextInt();
-		System.out.println(input + " * 1 = " + input * 1);
-		System.out.println(input + " * 2 = " + input * 2);
-		System.out.println(input + " * 3 = " + input * 3);
-		System.out.println(input + " * 4 = " + input * 4);
-		System.out.println(input + " * 5 = " + input * 5);
-		System.out.println(input + " * 6 = " + input * 6);
-		System.out.println(input + " * 7 = " + input * 7);
-		System.out.println(input + " * 8 = " + input * 8);
-		System.out.println(input + " * 9 = " + input * 9);
+//		// Two times table
+//		System.out.println("Two times table: ");
+//		System.out.println("2 * 1 = " + 2 * 1);
+//		System.out.println("2 * 2 = " + 2 * 2);
+//		System.out.println("2 * 3 = " + 2 * 3);
+//		System.out.println("2 * 4 = " + 2 * 4);
+//		System.out.println("2 * 5 = " + 2 * 5);
+//		System.out.println("2 * 6 = " + 2 * 6);
+//		System.out.println("2 * 7 = " + 2 * 7);
+//		System.out.println("2 * 8 = " + 2 * 8);
+//		System.out.println("2 * 9 = " + 2 * 9);
+//		System.out.println("");
+//
+//		// Three times table
+//		System.out.println("Three times table: ");
+//		System.out.println("3 * 1 = " + 3 * 1);
+//		System.out.println("3 * 2 = " + 3 * 2);
+//		System.out.println("3 * 3 = " + 3 * 3);
+//		System.out.println("3 * 4 = " + 3 * 4);
+//		System.out.println("3 * 5 = " + 3 * 5);
+//		System.out.println("3 * 6 = " + 3 * 6);
+//		System.out.println("3 * 7 = " + 3 * 7);
+//		System.out.println("3 * 8 = " + 3 * 8);
+//		System.out.println("3 * 9 = " + 3 * 9);
+		
+//		// Three times table
+//		System.out.println("Three times table: ");
+//		System.out.println("3 * 1 = " + 3 * 1);
+//		System.out.println("3 * 2 = " + 3 * 2);
+//		System.out.println("3 * 3 = " + 3 * 3);
+//		System.out.println("3 * 4 = " + 3 * 4);
+//		System.out.println("3 * 5 = " + 3 * 5);
+//		System.out.println("3 * 6 = " + 3 * 6);
+//		System.out.println("3 * 7 = " + 3 * 7);
+//		System.out.println("3 * 8 = " + 3 * 8);
+//		System.out.println("3 * 9 = " + 3 * 9);
+
+//		// four times table
+//		int result = 4 * 1;
+//		System.out.println(result);
+//		result = 4 * 2;
+//		System.out.println(result);
+//		result = 4 * 3;
+//		System.out.println(result);
+//		result = 4 * 4;
+//		System.out.println(result);
+//		result = 4 * 5;
+//		System.out.println(result);
+//		result = 4 * 6;
+//		System.out.println(result);
+//		result = 4 * 7;
+//		System.out.println(result);
+//		result = 4 * 8;
+//		System.out.println(result);
+//		result = 4 * 9;
+//		System.out.println(result);
+		
+		
+//		System.out.println("구구단 중 출력하고 싶은 단은? ");
+//		Scanner scanner = new Scanner(System.in);
+//		int input = scanner.nextInt();
+//		System.out.println(input + " * 1 = " + input * 1);
+//		System.out.println(input + " * 2 = " + input * 2);
+//		System.out.println(input + " * 3 = " + input * 3);
+//		System.out.println(input + " * 4 = " + input * 4);
+//		System.out.println(input + " * 5 = " + input * 5);
+//		System.out.println(input + " * 6 = " + input * 6);
+//		System.out.println(input + " * 7 = " + input * 7);
+//		System.out.println(input + " * 8 = " + input * 8);
+//		System.out.println(input + " * 9 = " + input * 9);
+		
+		// six time table
+		int i = 1; // starting point of iteration
+		while(i < 10) {
+			System.out.println("6 * " + i + " = "+ 6 * i);
+			i = i + 1;
+		}
+		
+		// seven time table
+		for(int j = 1; j < 10; j++) {
+			System.out.println("7 * " + j + " = " + 7 * j);
+		}
 	}
 }
